8b4c7e9ae615c0a3c5e53d1bfcef4406d50bd21a
dong.tqt <dong.tqt@ikigroup.vn>
2024-02-22 10:25:04 +0700

    correct import path

--- src/modules/redeem/redeem.module.ts
+++ src/modules/redeem/redeem.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { RedeemController } from "./controllers/redeem.controller";
 import { RedeemHelper } from "./helpers/redeem.helper";
 import { RedeemMissionHelper } from "./helpers/redeem_mission.helper";
-import { Redeem, RedeemSchema } from "./schemas/Redeem.schema";
+import { Redeem, RedeemSchema } from "./schemas/redeem.schema";
 import { RedeemMission, RedeemMissionSchema } from "./schemas/redeem_mission.schema";
 import { RedeemService } from "./services/redeem.service";
 import { RedeemMissionService } from "./services/redeem_mission.service";
