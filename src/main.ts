95b008fc8b6b0c1ca953497d60491a1270bafcf7
dong.tqt <dong.tqt@ikigroup.vn>
2024-02-27 14:29:09 +0700

    allow class_id header

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
     res.header(
       "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, X-Channel, X-Authorization, Content-Type, Accept"
+      "Origin, X-Requested-With, X-Channel, X-Authorization, Content-Type, Accept, Class-Id"
     );
     res.header("Access-Control-Allow-Credentials", "true");
     if (req.method == "OPTIONS") {
