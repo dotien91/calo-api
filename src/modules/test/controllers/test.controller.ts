82f5966137072297ecb2dc519774e5e250b701cb
dong.tqt <dong.tqt@ikigroup.vn>
2024-03-26 16:08:23 +0700

    update test cron time

--- src/modules/test/controllers/test.controller.ts
+++ src/modules/test/controllers/test.controller.ts
@@ -20,7 +20,7 @@ export class TestController {
     private readonly testUserHelper: TestUserHelper
   ) {}
 
-  @Cron(CronExpression.EVERY_MINUTE)
+  @Cron(CronExpression.EVERY_10_MINUTES)
   async checkUserTestSubmit() {
     try {
       const tests = await this.testUserHelper.getTestByStatus(TestStatus.PENDING);
